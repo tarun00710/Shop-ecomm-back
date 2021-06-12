cefab9d5fd3460cb06799f71be944c082f103fd5
tarun00710 <tarunlal07@gmail.com>
2021-06-12 10:12:42 +0530

    UPDATE
    updated routes

--- Routes/user-route.js
+++ Routes/user-route.js
@@ -203,6 +203,36 @@ router.post('/:userId/updatecart/:productId',userIdCheckHandler,async(req, res)=
         res.json({success:false,message:"Failed to move to wishlist",error})
     }
 })
+//IncreaseOrdecreaseQuantity
+router.post('/:userId/cart/:productId/update/:type',userIdCheckHandler,async(req,res)=>{
+    try{
+        const{userId,productId,type}=req.params;
+        console.log(type)
+        const updateQty=await User.findByIdAndUpdate(userId,{"cart":[{product:productId}]}).select("cart.quantity")
+        console.log(updateQty.cart[0].quantity)
+        if(type==="increaseQuantity"){
+            console.log("here")
+            const updateProductQuantity=await User.findByIdAndUpdate(userId,
+                {
+                    "cart":[{product:productId,quantity:updateQty.cart[0].quantity+1}]
+                },{new: true}
+                ).select("cart").populate("cart.product","-__v")
+                res.json({success:true,updateProductQuantity})
+        }
+        else{
+            console.log("not here")
+            const updateProductQuantity=await User.findByIdAndUpdate(userId,
+                {
+                    "cart":[{product:productId,quantity:updateQty.cart[0].quantity-1}]
+                }
+                ,{new: true}
+                ).select("cart").populate("cart.product","-__v")
+                res.json({success:true,updateProductQuantity})
+        } 
+    }catch(err){
+        res.json({success:false,message:err})
+    }
+})
 
 
 module.exports=router;
